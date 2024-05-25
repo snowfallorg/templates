6788a802fa517276140d30a88d6e97ed22f76c73
Jake Hamilton <jake.hamilton@hey.com>
2024-05-25 15:23:16 -0700

    feat: use snowfall lib for templates

--- flake.nix
+++ flake.nix
@@ -1,39 +1,46 @@
 {
   description = "Snowfall Templates";
 
-  outputs = { self }: {
-    templates = rec {
-      default = empty;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
-      empty = {
-        path = ./empty;
-        description = "A Nix Flake using snowfall-lib";
-      };
-
-      system = {
-        path = ./system;
-        description = "A Nix flake with a NixOS system and modules ready to modify.";
-      };
-
-      package = {
-        path = ./package;
-        description = "A Nix Flake that exports packages and an overlay.";
-      };
-
-      module = {
-        path = ./module;
-        description = "A Nix Flake that exports NixOS modules.";
-      };
-
-      lib = {
-        path = ./lib;
-        description = "A Nix Flake that exports a custom lib.";
-      };
-
-      home = {
-        path = ./home;
-        description = "A Nix Flake that exports home manager.";
-      };
+    snowfall-lib = {
+      url = "github:snowfallorg/lib?ref=v3.0.2";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      src = ./.;
+
+      alias.templates.default = "empty";
+
+      templates = rec {
+        empty = {
+          description = "A Nix Flake using snowfall-lib";
+        };
+
+        system = {
+          description = "A Nix flake with a NixOS system and modules ready to modify.";
+        };
+
+        package = {
+          description = "A Nix Flake that exports packages and an overlay.";
+        };
+
+        module = {
+          description = "A Nix Flake that exports NixOS modules.";
+        };
+
+        lib = {
+          description = "A Nix Flake that exports a custom lib.";
+        };
+
+        home = {
+          description = "A Nix Flake that exports home manager.";
+        };
+      };
+    };
 }
